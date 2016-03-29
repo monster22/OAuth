9b89858d87658e562693161f94bca2166d67ae66
monster22 <coolasifali786@gmail.com>
2016-03-29 20:38:56 +0530

    First Commit

---
+++ app/routes/auth.js
@@ -0,0 +1,32 @@
+module.exports = function(router, passport){
+	router.get('/', function(req, res){
+		res.render('index.ejs');
+	});
+
+	router.get('/login', function(req, res){
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+	router.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/auth/login',
+		failureFlash: true
+	}));
+
+	router.get('/signup', function(req, res){
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+
+	router.post('/signup', passport.authenticate('local-signup', {
+		successRedirect: '/auth/login',
+		failureRedirect: '/auth/signup',
+		failureFlash: true
+	}));
+
+
+
+	router.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	});
+};
